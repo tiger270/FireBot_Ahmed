15ab310e91144375231ce355b8be88d804ec4752
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2019-01-06 21:27:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -856,7 +856,7 @@ return message.reply("**لا يمكنك تغيير الاسم يجب عليك ا
 client.user.setAvatar(argresult);
   message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
       } else     
-if (message.content.startsWith(adminprefix + 'setT')) {
+if (message.content.startsWith(adminprefix + 'setS')) {
   client.user.setGame(argresult, "https://www.twitch.tv/idk");
     message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
