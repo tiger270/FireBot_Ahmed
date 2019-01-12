fee33d60d8b8036411222ac91df2911383e888f9
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2019-01-12 11:42:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2893,24 +2893,5 @@ client.on('ready', () => {//new ready event
   }, 5000);//the rainbow time
 })
 
-
-client.on('guildMemberAdd', Sal => { //By Salto7#4595
-    var embed = new Discord.RichEmbed()
-    .setAuthor(Sal.user.username, Sal.user.avatarURL)
-    .setThumbnail(Sal.user.avatarURL)
-    .setImage('https://cdn.discordapp.com/attachments/492862340484694027/493771573740830740/welcome1.png'
-    .setTitle('عضو جديد!')
-    .setDescription('مرحبا بك بالسيرفر')
-    .addField('``ايدي العضو``:',"" +  Sal.user.id, true)
-    .addField('``تاق العضو``', Sal.user.discriminator, true)
-    .addField('``تم الانشاء في``', Sal.user.createdAt, true)
-    .addField(' 👤  انت رقم',`**[ ${Sal.guild.memberCount} ]**`,true)
-    .setColor('RANDOM')
-    .setFooter(Sal.guild.name, Sal.guild.iconURL, true)
-    var channel =Sal.guild.channels.find('name', ' (welcome)')
-    if (!channel) return;
-    channel.send({embed : embed});
-    });
-
 //MHSTR END NOW THIS IS END
 client.login(process.env.BOT_TOKEN)
